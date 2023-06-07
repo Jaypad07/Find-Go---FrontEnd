64ec0c5400cd36a15686fea1a62e4da824a66b8d
jaypad07 <jaypad07@gmail.com>
2023-06-07 01:17:13 -0700

    added routing and new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterUserComponent } from './register-user/register-user.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RegisterUserComponent } from './register-user/register-user.component';
 import { StoreListingComponent } from './store-listing/store-listing.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: StoreListingComponent }, // Default route
+  { path: 'register', component: RegisterUserComponent },
+  { path: 'login', component: LoginPageComponent },
+  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    LoginPageComponent,
     RegisterUserComponent,
+    LoginPageComponent,
     StoreListingComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     NgbModule,
     FormsModule,
     HttpClientModule,
