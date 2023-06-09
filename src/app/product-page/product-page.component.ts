5ec9323d5b6730ec6a14604771b0bb302ccee17e
jaypad07 <jaypad07@gmail.com>
2023-06-08 18:27:31 -0700

    added searchbar

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product.models';
 import { ApiService } from '../api.service';
+import { Store } from '../models/store.models';
 
 @Component({
   selector: 'app-product-page',
@@ -9,10 +10,15 @@ import { ApiService } from '../api.service';
 })
 export class ProductPageComponent {
   productList: Product[] = [];
+  sectionList: Store[] = [];
   
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getStoreById(1)
+    .subscribe((data: any) => {this.sectionList = data.storeSections;
+       console.log(data);});
+
     this.apiService.getProducts()
     .subscribe((products: any) => {this.productList = products
       console.log(products);});
